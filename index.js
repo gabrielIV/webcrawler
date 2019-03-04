abd92f41ff6b5153a3249f19e9bddb339b243a9e
gabrielIV <gabrielkamau9@gmail.com>
2019-03-04 20:06:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const request = require("request");
+
+const cheerio = require("cheerio");
+const $ = cheerio.load('<h2 class="title">Hello world</h2>');
+
+console.log("fetching data ....");
+url =
+  "https://www.jumia.co.ke/?gclid=Cj0KCQiAtvPjBRDPARIsAJfZz0qIcTCtIX-kledq15gjQgvurxovJIk0y0gDOCrFTPKZnGGbm3uRoSMaAnu8EALw_wcBt";
+
+request(url, { json: false }, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  let categories = {};
+
+  const $ = cheerio.load(body);
+  let l = $(body)
+    .find(".menu-item")
+    .each((n, v) => {
+      let t = $(".nav-subTxt").text();
+      if (!categories[t]) {
+        categories[t] = [];
+      }
+
+      let subcategory = {};
+      $(v)
+        .find(".categories")
+        .each((num, val) => {
+          let title = $(val)
+            .find(".category")
+            .text();
+          if (!subcategory[title]) {
+            subcategory[title] = [];
+          }
+          $(v)
+            .find(".subcategory")
+            .each((n1, v1) => {
+              let value = $(v1).text();
+              subcategory[title].push(value);
+            });
+        });
+
+      categories[t].push(subcategory);
+    });
+  console.log(categories);
+});
